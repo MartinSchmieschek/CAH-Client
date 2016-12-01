213a8d96ed0a6f74b3d31d8bf7b7d56d3eb15c28
Martin Schmieschek <martin.schmieschek@googlemail.com>
2016-12-02 00:05:36 +0100

    neue animation für den titel im main menu

---
+++ Assets/TitleScreenController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenController : MonoBehaviour
+{
+
+    public Animation Text;
+    public Animation TextBackground;
+
+    public void Awake()
+    {
+        Text.Stop();
+        TextBackground.Stop();
+    }
+
+    public void Start()
+    {
+        StartCoroutine(Appear());
+    }
+
+    public void OnDestroy()
+    {
+
+    }
+
+    IEnumerator Appear()
+    {
+        TextBackground.Play();
+        yield return new WaitForSeconds(1);
+        Text.Play();
+        yield return null;
+    }
+}
