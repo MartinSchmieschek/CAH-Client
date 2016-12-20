ac85bc46a94c7f49d8f511b8ffb2ccd1f03b4515
Martin Schmieschek <martin.schmieschek@googlemail.com>
2016-12-21 00:22:59 +0100

    no message

--- Assets/ScreenSwitcher.cs
+++ Assets/ScreenSwitcher.cs
@@ -5,29 +5,28 @@ using System.Collections;
 public struct ScreenItems
 {
     public string Name;
+    public Camera GameObject;
 }
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Camera))]
 public class ScreenSwitcher : MonoBehaviour
 {
 
     public ScreenItems[] Screens;
-    private Animator animator;
-
-    public void Awake()
-    {
-        animator = GetComponent<Animator>();
-
-        if (animator == null)
-            throw new System.Exception("No Animator found !");
-    }
 
     public void SwitchTo(string name)
     {
         for (int i = 0; i < Screens.Length; i++)
         {
             if (string.Equals(name, Screens[i].Name))
-                animator.SetInteger("ScreenId", i);
+            {
+                if (Screens[i].GameObject != null)
+                {
+                    transform.position = Screens[i].GameObject.transform.position;
+                    transform.rotation = Screens[i].GameObject.transform.rotation;
+                }
+            }
+                
         }
     }
 }
